9fb7aa929c459d264679a89d3ab3b55b9a9f6143
Valentyna Chernova <valentyna19960410@gmail.com>
2025-11-30 15:37:02 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
 // write code here
+const items = document.querySelectorAll('.tree > li');
+
+items.forEach(li => {
+  const firstTextNode = li.childNodes[0];
+  const span = document.createElement('span');
+  span.textContent = firstTextNode.textContent;
+  li.replaceChild(span, firstTextNode);
+
+  const nextItems = li.querySelectorAll('ul > li');
+
+nextItems.forEach(nextLi => {
+  if (nextLi.querySelector('ul')) {
+     const nestedFirstTextNode = nextLi.childNodes[0];
+  const nestedSpan = document.createElement('span');
+  nestedSpan.textContent = nestedFirstTextNode.textContent;
+  nextLi.replaceChild(nestedSpan, nestedFirstTextNode);
+  }
+
+});
+
+});
+
+const spans = document.querySelectorAll('span');
+
+spans.forEach(span => span.addEventListener('click', (e) => {
+  const li = e.target.closest('li');
+  const nestedUl = li.querySelector(':scope > ul');
+   if (!nestedUl) return;
+    nestedUl.style.display = nestedUl.style.display === 'none'? 'block': 'none';
+
+  }
+  )
+)
